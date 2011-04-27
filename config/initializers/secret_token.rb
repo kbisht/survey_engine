9565a8a8f0aafdc0fd6c917472b8bf7b0b21f1b9
kumar <ks.bisht@hotmail.com>
2011-04-27 12:02:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyEngine::Application.config.secret_token = '9972118d4f88fae7b4c6e99719ffcd836a35615252ba6df725313952aa01f913659d6899aeb8e57fba8f519d8a0f03b7e1e351cb414f8e47ee1ab9b478c5b9a3'
