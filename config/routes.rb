967952226d86f2a56d65442fe4676797168000d6
kumar <ks.bisht@hotmail.com>
2011-04-29 12:32:10 -0500

    finished with first cut of the user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SurveyEngine::Application.routes.draw do
  
-  get "users/new"
+  resources :users
+	
 
   match '/signup'  ,      :to => 'users#new'
   match '/contact' ,      :to => 'pages#Contact'
