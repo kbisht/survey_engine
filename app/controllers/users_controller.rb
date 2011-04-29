a1c11b635458391425356868dd41bedcfb645e92
kumar <ks.bisht@hotmail.com>
2011-04-29 15:51:09 -0500

    user signup complete with a glictch remaining for chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,21 @@ class UsersController < ApplicationController
 
 
   def new
-
+	@user = User.new
 	@title = "Sign Up"
   end
+
+  def create
+	@user = User.new(params[:user])
+
+	if @user.save
+		flash[:success] = "Success!"
+		redirect_to @user
+	else
+		@title = "Sign up"
+		render 'new'
+	end
+  end
+	
 
 end
